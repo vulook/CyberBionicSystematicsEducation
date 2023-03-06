396724734bd72b5e6282feb90b96b0859c743fb0
vulook <andriy.dep@gmail.com>
2023-03-06 21:08:04 +0200

    Task04

--- src/homework015/Task04.java
+++ src/homework015/Task04.java
@@ -1,5 +1,7 @@
 package homework015;
 
+import java.util.Scanner;
+
 /**
  * FileName: Task03
  * Author:   Andriy Vulook
@@ -7,5 +9,77 @@ package homework015;
  * Description: Task04
  */
 
+class DocumentWorker {
+
+    public void openDocument() {
+        System.out.println("The document opened.");
+    }
+
+    public void editDocument() {
+        System.out.println("Document edits are available in the PRO-version.");
+    }
+
+    public void saveDocument() {
+        System.out.println("Document saves are available in the PRO-version.");
+    }
+}
+
+class ProDocumentWorker extends DocumentWorker {
+
+    @Override
+    public void editDocument() {
+        System.out.println("The document was edited.");
+    }
+
+    @Override
+    public void saveDocument() {
+        System.out.println("The document was saved in a OLD format.");
+        System.out.println("Other save formats are available in the EXPERT-version.");
+    }
+}
+
+class ExpertDocumentWorker extends DocumentWorker {
+
+    @Override
+    public void editDocument() {
+        System.out.println("The document was edited.");
+    }
+
+    @Override
+    public void saveDocument() {
+        System.out.println("The document was saved in a NEW format.");
+    }
+}
+
 public class Task04 {
+
+    public static void main(String[] args) {
+
+        String key;
+        DocumentWorker dw;
+
+        System.out.println("This program edits and saves documents...");
+        System.out.println("Enter the KEY to unlock more functionality: PRO | EXP");
+        System.out.println("By default, the key is FREE.");
+        System.out.print("==>");
+
+        Scanner scanner = new Scanner(System.in);
+        key = scanner.nextLine();
+
+        switch (key) {
+            case "PRO":
+                dw = new ProDocumentWorker();
+                break;
+            case "EXP":
+                dw = new ExpertDocumentWorker();
+                break;
+            default:
+                dw = new DocumentWorker();
+        }
+
+        dw.openDocument();
+        dw.editDocument();
+        dw.saveDocument();
+    }
+
 }
