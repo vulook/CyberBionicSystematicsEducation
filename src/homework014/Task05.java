34e1acd104b84aa73108092e6d58c745fe7ff138
vulook <andriy.dep@gmail.com>
2023-03-07 15:13:41 +0200

    homework014

---
+++ src/homework014/Task05.java
@@ -0,0 +1,31 @@
+package homework014;
+
+/**
+ * FileName: Task05
+ * Author:   Andriy Vulook
+ * Date:     07.03.2023 14:21
+ * Description: MyArea
+ */
+
+class MyArea {
+    private static final float PI_NUMBER = 3.14F;
+
+    private static double r;
+
+    public static void areaOfCircle(double r) {
+        double area = r * r * PI_NUMBER;
+        System.out.println("The area of the circle [radius = " + r + "]: " + area);
+    }
+
+}
+
+public class Task05 {
+
+    public static void main(String[] args) {
+
+        double radius = Math.random();
+        MyArea.areaOfCircle(radius);
+
+    }
+
+}
