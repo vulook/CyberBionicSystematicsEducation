0db792d14d36b38911066822d8a6a42f055c024b
vulook <andriy.dep@gmail.com>
2023-03-10 19:41:43 +0200

    Task03

--- src/homework019/Task03.java
+++ src/homework019/Task03.java
@@ -4,9 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.ToString;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 /**
  * FileName: Task03
@@ -22,8 +20,15 @@ class Price {
     private String productName;
     private String storeName;
     private double price;
+
 }
 
+class SortByStoreName implements Comparator<Price> {
+    @Override
+    public int compare(Price a, Price b) {
+        return a.getStoreName().compareTo(b.getStoreName());
+    }
+}
 
 public class Task03 {
 
@@ -31,22 +36,65 @@ public class Task03 {
 
         String inputProduct;
         String inputStore;
+        String searchStore;
         double inputPrice;
+
         List<Price> prices = new ArrayList<>();
         Scanner s = new Scanner(System.in);
 
         for (int i = 0; i < 4; i++) {
-            System.out.println("Enter product details No.: " + (i+1));
+            System.out.println("Enter product details No.: " + (i + 1));
             System.out.print("Product name: ");
             inputProduct = s.nextLine();
             System.out.print("Store name: ");
             inputStore = s.nextLine();
             System.out.print("Price in UAH: ");
             inputPrice = s.nextDouble();
+            s.nextLine();
             prices.add(new Price(inputProduct, inputStore, inputPrice));
+        }
+
+        prices.sort(new SortByStoreName());
+        printList(prices);
+
+
+        System.out.println("Search by store:");
+        searchStore = s.nextLine();
+
+        try {
+            printSearchStore(prices, searchStore);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
         }
 
     }
 
+    static void printList(List<Price> prices) {
+        System.out.println("\n");
+        for (Price price : prices) {
+            System.out.println((prices.indexOf(price) + 1) + ". "
+                    + price.getProductName() + ", "
+                    + price.getStoreName() + ", "
+                    + price.getPrice());
+        }
+        System.out.println("\n");
+    }
+
+    static void printSearchStore(List<Price> prices, String searchStore) throws IllegalArgumentException {
+        boolean flag = false;
+
+        for (Price price : prices) {
+            if (price.getStoreName().equalsIgnoreCase(searchStore)) {
+                System.out.println((prices.indexOf(price) + 1) + ". "
+                        + price.getProductName() + ", "
+                        + price.getPrice() + " UAH");
+                flag = true;
+            }
+        }
+
+        if (!flag) {
+            throw new IllegalArgumentException("Store not found.");
+        }
+    }
 
 }
