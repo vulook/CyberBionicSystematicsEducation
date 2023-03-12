f42aad1b2dda0e12a318012c9c27bce4298ecb5f
vulook <andriy.dep@gmail.com>
2023-03-12 02:14:16 +0200

    Task02

--- src/homework020/Task02.java
+++ src/homework020/Task02.java
@@ -1,7 +1,5 @@
 package homework020;
 
-import lombok.*;
-
 /**
  * FileName: Task02
  * Author:   Andriy Vulook
@@ -11,24 +9,65 @@ import lombok.*;
  * 2) Subclass Monitor (resolutionX(int), resolutionY(int)) and EthernetAdapter (speed(int), mac(String));
  */
 
-@Getter
-@Setter
-@AllArgsConstructor //generates a constructor with all parameters
-@ToString
+
 class Device {
 
     private String manufacturer;
     private float price;
     private String serialNumber;
 
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public String getSerialNumber() {
+        return serialNumber;
+    }
+
+    public void setManufacturer(String manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public void setSerialNumber(String serialNumber) {
+        this.serialNumber = serialNumber;
+    }
+
+    public Device(String manufacturer, float price, String serialNumber) {
+        this.manufacturer = manufacturer;
+        this.price = price;
+        this.serialNumber = serialNumber;
+    }
 }
 
-@Getter
-@Setter
+
 class Monitor extends Device {
 
     private int resolutionX;
     private int resolutionY;
+
+    public int getResolutionX() {
+        return resolutionX;
+    }
+
+    public int getResolutionY() {
+        return resolutionY;
+    }
+
+    public void setResolutionX(int resolutionX) {
+        this.resolutionX = resolutionX;
+    }
+
+    public void setResolutionY(int resolutionY) {
+        this.resolutionY = resolutionY;
+    }
 
     public Monitor(String manufacturer, float price, String serialNumber, int resolutionX, int resolutionY) {
         super(manufacturer, price, serialNumber);
@@ -38,12 +77,27 @@ class Monitor extends Device {
     }
 }
 
-@Getter
-@Setter
+
 class EthernetAdapter extends Device {
 
     private int speed;
     private String mac;
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public String getMac() {
+        return mac;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+
+    public void setMac(String mac) {
+        this.mac = mac;
+    }
 
     public EthernetAdapter(String manufacturer, float price, String serialNumber, int speed, String mac) {
         super(manufacturer, price, serialNumber);
@@ -54,22 +108,5 @@ class EthernetAdapter extends Device {
 }
 
 public class Task02 {
-
-    public static void main(String[] args) {
-
-        Monitor monitor = new Monitor("Dell", 320, "123456789", 1920, 1080);
-        EthernetAdapter adapter = new EthernetAdapter("Starlink", 2000, "8888888888", 1000, "ce:5f:65:b0:a9:35");
-
-        System.out.println(monitor.getManufacturer() + ", resolution:" + monitor.getResolutionX() + "x" + monitor.getResolutionY());
-        monitor.setResolutionX(2560);
-        monitor.setResolutionY(1440);
-        System.out.println(monitor.getManufacturer() + ", resolution:" + monitor.getResolutionX() + "x" + monitor.getResolutionY());
-
-        System.out.println(adapter.getManufacturer() + ", speed:" + adapter.getSpeed());
-        adapter.setManufacturer("Starlink by SpaceX");
-        adapter.setSpeed(3000);
-        System.out.println(adapter.getManufacturer() + ", speed:" + adapter.getSpeed());
-
-    }
-
+    
 }
