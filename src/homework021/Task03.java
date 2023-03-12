721e720e9cb80d99918bc30cea7f4cadf52c09e5
vulook <andriy.dep@gmail.com>
2023-03-12 16:19:40 +0200

    Task03

---
+++ src/homework021/Task03.java
@@ -0,0 +1,11 @@
+package homework021;
+
+/**
+ * FileName: Task03
+ * Author:   Andriy Vulook
+ * Date:     12.03.2023 16:13
+ * Description: Create a MyDictionary class. Implement in the simplest approximation the possibility of using its instance.
+ */
+
+public class Task03 {
+}
